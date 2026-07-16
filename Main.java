4f8a41279aa6ecdd7226a03b7408d765f7ff2409
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-16 17:18:20 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+// File: com/test/Main.java
+package myclass;
+
+
+public class Main {
+public static void main(String[] args) {
+MyClass myClass = new MyClass();
+myClass.display(); // Output: Hello from MyClass!
+}
+}
---
+++ MyClass.java
@@ -0,0 +1,8 @@
+// File: com/example/MyClass.java
+package myclass;
+
+public class MyClass {
+public void display() {
+System.out.println("\033c\033[47;30m\nHello from MyClass!");
+}
+}
